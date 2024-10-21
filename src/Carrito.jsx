4a582c80983faaf7f3d5c441aa7d7cd9e59cb217
Leonardo Pereyra <leonralph3d@gmail.com>
2024-10-21 17:55:53 -0500

    prueba de ruta netlify

--- src/Carrito.jsx
+++ src/Carrito.jsx
@@ -58,7 +58,7 @@ function Carrito() {
                 <AgregarProducto setAllProducts={setAllProducts} 
                 setMostrarAgregarProducto={setMostrarAgregarProducto}/>
             )}
-            <div><p>Hola mundo</p></div>
+            <div><p>Hola tony cacherin</p></div>
         </>
     );
 }
