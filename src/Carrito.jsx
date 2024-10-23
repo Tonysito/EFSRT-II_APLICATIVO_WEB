ce73a1bd1f11d89ffee790ee82e2ee6f1edd26f4
Leonardo Pereyra <leonralph3d@gmail.com>
2024-10-22 22:00:51 -0500

    modificacion para netlify

--- src/Carrito.jsx
+++ src/Carrito.jsx
@@ -58,7 +58,7 @@ function Carrito() {
                 <AgregarProducto setAllProducts={setAllProducts} 
                 setMostrarAgregarProducto={setMostrarAgregarProducto}/>
             )}
-            
+            <div><p>Prueba 1</p></div>
         </>
     );
 }
